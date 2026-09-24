ea2a06ce84b75ccd222356d7d8d78347105e7778
Gerardo Rodrigues <gerardofilho56@gmail.com>
2026-09-24 12:01:57 -0300

    exibindo os detalhes de cada tarefa, e verificando se existe mudancas

--- frontend/src/app/features/project/project.routes.ts
+++ frontend/src/app/features/project/project.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { projectResolver } from '../../core/resolvers/project-resolver';
 import { projectTasksResolver } from '../../core/resolvers/project-tasks-resolver';
+import { taskResolver } from '../../core/resolvers/task-resolver';
+import { unsavedChangesGuard } from '../../core/guards/unsaved-changes-guard';
 
 export const PROJECT_ROUTES: Routes = [
   {
@@ -36,6 +38,8 @@ export const PROJECT_ROUTES: Routes = [
       {
         path: 'task/:taskId',
         outlet: 'details',
+        resolve: { task: taskResolver },
+        canDeactivate: [unsavedChangesGuard],
         loadComponent: () => import('./task-detail/task-detail').then((m) => m.TaskDetail),
         title: 'Detalhes da Task',
       },
