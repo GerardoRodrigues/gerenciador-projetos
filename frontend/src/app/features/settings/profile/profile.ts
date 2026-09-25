69f2e451408d7eb8b25ad2656d891dc405a395b9
Gerardo Rodrigues <gerardofilho56@gmail.com>
2026-09-25 11:36:27 -0300

    roteamento na parte de configuracoes

--- frontend/src/app/features/settings/profile/profile.ts
+++ frontend/src/app/features/settings/profile/profile.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { AuthManager } from '../../../core/services/auth-manager';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './profile.html'
 })
 export class Profile {
